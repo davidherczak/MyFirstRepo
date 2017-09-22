dc8440605fa4f6085756823517a72211f46f425e
W10-TP-E460\David Herczakowski <david.herczakowski@hakmistelbach.ac.at>
2017-09-22 09:13:22 +0200

    Button hinzugefügt

--- MyFirstRepo/Form1.Designer.cs
+++ MyFirstRepo/Form1.Designer.cs
@@ -28,13 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(145, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click me!!!";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(506, 398);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "MyFirstRepo";
             this.ResumeLayout(false);
@@ -42,6 +56,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
